6c51230f72938a890d0f3c35a3473486abd99c4a
Ivan <paramonikhin.ivan@yandex.ru>
2023-06-06 21:15:20 +0300

    Initial Commit

---
+++ toDo_list_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDo_list_MVC
+//
+//  Created by Иван on 06.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
